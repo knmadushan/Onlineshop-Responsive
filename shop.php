0f957958f239635aba3bfb28a36a6c24ae4b3655
K.N Madushan <115458747+knmadushan@users.noreply.github.com>
2023-04-08 19:24:05 +0530

    Add files via upload

---
+++ shop.php
@@ -0,0 +1,310 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <style>
+        .row .col-12 .card-wrp {
+            border-radius: 10px;
+            border-style: solid;
+        }
+
+        .col-12 .card-wrp {
+            padding: 16px;
+            border: rgba(192, 192, 192);
+            margin: 10px auto;
+        }
+        .container-fluid{
+            background-color: #00000044;
+            padding: 10px;
+        }
+        footer .quick-link-wrp .content-wrp .link-List ul li a{
+            text-decoration: none;
+        }
+    </style>
+
+
+</head>
+
+<body>
+
+    <nav class="navbar navbar-dark bg-dark navbar-expand-sm">
+        <div class="container">
+            <a href="" class="navbar-brand">brand</a>
+            <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navi"><span class="navbar-toggler-icon"></span></button>
+
+            <div class="collapse navbar-collapse justify-content-center" id="navi">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a href="" class="nav-link active">Home</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Pricing</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Contact Us</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Gallery</a></li>
+                </ul>
+            </div>
+            <div class="right-pane">
+                <div class="d-flex">
+                    <div class="cart">
+                        <a href=""><i class="fa-solid fa-cart-shopping"></i></a>
+                    </div>
+                    <div class="login ms-3">
+                        <a href="">Login</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <div class="container-fluid px-0">
+        <div class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active">
+                    <img src="image/1.jpg" alt="" class="d-block w-100">
+                </div>
+                <div class="carousel-item">
+                    <img src="image/2.jpg" alt="" class="d-block w-100">
+                </div>
+                <div class="carousel-item">
+                    <img src="image/3.jpg" alt="" class="d-block w-100">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center">Product</h1>
+            </div>
+        </div>
+        <!-- product -->
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+                <div class="card-wrp">
+                    <div class="img-wrp">
+                        <img src="image/Products/product-01.webp" alt="" class="mx-auto d-block img-fluid">
+                    </div>
+                    <div class="product-name">
+                        Product Name
+                    </div>
+                    <div class="product-details d-flex justify-content-between">
+                        <span>Price</span>
+                        <span>$50</span>
+                    </div>
+                    <div class="button">
+                        <button class="btn btn-primary me-2">Buy Now</button>
+                        <BUtton class="btn btn-link btn-floating"> <i class="fa-solid fa-cart-shopping"></i></BUtton>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+                <div class="card-wrp">
+                    <div class="img-wrp">
+                        <img src="image/Products/product-01.webp" alt="" class="mx-auto d-block img-fluid">
+                    </div>
+                    <div class="product-name">
+                        Product Name
+                    </div>
+                    <div class="product-details d-flex justify-content-between">
+                        <span>Price</span>
+                        <span>$50</span>
+                    </div>
+                    <div class="button">
+                        <button class="btn btn-primary me-2">Buy Now</button>
+                        <BUtton class="btn btn-link btn-floating"> <i class="fa-solid fa-cart-shopping"></i></BUtton>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+                <div class="card-wrp">
+                    <div class="img-wrp">
+                        <img src="image/Products/product-01.webp" alt="" class="mx-auto d-block img-fluid">
+                    </div>
+                    <div class="product-name">
+                        Product Name
+                    </div>
+                    <div class="product-details d-flex justify-content-between">
+                        <span>Price</span>
+                        <span>$50</span>
+                    </div>
+                    <div class="button">
+                        <button class="btn btn-primary me-2">Buy Now</button>
+                        <BUtton class="btn btn-link btn-floating"> <i class="fa-solid fa-cart-shopping"></i></BUtton>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+                <div class="card-wrp">
+                    <div class="img-wrp">
+                        <img src="image/Products/product-01.webp" alt="" class="mx-auto d-block img-fluid">
+                    </div>
+                    <div class="product-name">
+                        Product Name
+                    </div>
+                    <div class="product-details d-flex justify-content-between">
+                        <span>Price</span>
+                        <span>$50</span>
+                    </div>
+                    <div class="button">
+                        <button class="btn btn-primary me-2">Buy Now</button>
+                        <BUtton class="btn btn-link btn-floating"> <i class="fa-solid fa-cart-shopping"></i></BUtton>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+                <div class="card-wrp">
+                    <div class="img-wrp">
+                        <img src="image/Products/product-01.webp" alt="" class="mx-auto d-block img-fluid">
+                    </div>
+                    <div class="product-name">
+                        Product Name
+                    </div>
+                    <div class="product-details d-flex justify-content-between">
+                        <span>Price</span>
+                        <span>$50</span>
+                    </div>
+                    <div class="button">
+                        <button class="btn btn-primary me-2">Buy Now</button>
+                        <BUtton class="btn btn-link btn-floating"> <i class="fa-solid fa-cart-shopping"></i></BUtton>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+                <div class="card-wrp">
+                    <div class="img-wrp">
+                        <img src="image/Products/product-01.webp" alt="" class="mx-auto d-block img-fluid">
+                    </div>
+                    <div class="product-name">
+                        Product Name
+                    </div>
+                    <div class="product-details d-flex justify-content-between">
+                        <span>Price</span>
+                        <span>$50</span>
+                    </div>
+                    <div class="button">
+                        <button class="btn btn-primary me-2">Buy Now</button>
+                        <BUtton class="btn btn-link btn-floating"> <i class="fa-solid fa-cart-shopping"></i></BUtton>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+                <div class="card-wrp">
+                    <div class="img-wrp">
+                        <img src="image/Products/product-01.webp" alt="" class="mx-auto d-block img-fluid">
+                    </div>
+                    <div class="product-name">
+                        Product Name
+                    </div>
+                    <div class="product-details d-flex justify-content-between">
+                        <span>Price</span>
+                        <span>$50</span>
+                    </div>
+                    <div class="button">
+                        <button class="btn btn-primary me-2">Buy Now</button>
+                        <BUtton class="btn btn-link btn-floating"> <i class="fa-solid fa-cart-shopping"></i></BUtton>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+                <div class="card-wrp">
+                    <div class="img-wrp">
+                        <img src="image/Products/product-01.webp" alt="" class="mx-auto d-block img-fluid">
+                    </div>
+                    <div class="product-name">
+                        Product Name
+                    </div>
+                    <div class="product-details d-flex justify-content-between">
+                        <span>Price</span>
+                        <span>$50</span>
+                    </div>
+                    <div class="button">
+                        <button class="btn btn-primary me-2">Buy Now</button>
+                        <BUtton class="btn btn-link btn-floating"> <i class="fa-solid fa-cart-shopping"></i></BUtton>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="py-0">
+        <div class="container-fluid px-0">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="content-wrp">
+                            <div class="img-wrp">
+                                <img src="image/logo.png" alt="" class="mx-auto d-block" width="150px">
+                            </div>
+                            <div class="product-describe">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nesciunt odit fugit harum esse iste veritatis,
+                                    eum iusto voluptates ipsa architecto rerum. At quae odit possimus, debitis voluptatem excepturi reprehenderit?</p>
+                            </div>
+                            <div class="social-icon">
+                                <div class="d-flex me-3">
+                                    <span class="me-2">
+                                        <i class="fa-brands fa-facebook"></i>
+                                    </span>
+                                    <span class="me-2">
+                                        <i class="fa-brands fa-instagram"></i>
+                                    </span>
+                                    <span class="me-2"> <i class="fa-brands fa-twitter"></i>
+                                </span>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col-md-3 quick-link-wrp">
+                        <div class="content-wrp">
+                            <div class="title">
+                                <h5>Quick Link</h5>
+                            </div>
+                            <div class="link-List">
+                                <ul>
+                                    <li><a href="">Home</a></li>
+                                    <li><a href="">Pricing</a></li>
+                                    <li><a href="">Contact Us</a></li>
+                                    <li><a href="">Gallery</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 quick-link-wrp">
+                        <div class="content-wrp">
+                            <div class="title">
+                                <h5> Quick Link</h5>
+                            </div>
+                            <div class="link-List">
+                                <ul>
+                                    <li><a href="">Product 1</a></li>
+                                    <li><a href="">Product 2</a></li>
+                                    <li><a href="">Product 3</a></li>
+                                    <li><a href="">Product 4</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="content-wrp">
+                            <div class="title">
+                                <h6>Subcribe for offer and details</h6>
+                            </div>
+                            <div class="form-outline mb-2">
+                                <input type="text" class="form-control" placeholder="example@gmail.com">
+                            </div>
+                            <div class="button">
+                                <button class="btn btn-primary">Subscribe</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.min.js" integrity="sha384-heAjqF+bCxXpCWLa6Zhcp4fu20XoNIA98ecBC1YkdXhszjoejr5y9Q77hIrv8R9i" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/88503f0b5b.js" crossorigin="anonymous"></script>
+
+</html>
